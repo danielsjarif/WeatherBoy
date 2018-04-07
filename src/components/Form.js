ec7d1e52fa727357f32290f4e8712d787338b5b6
Daniel Sjarif <dsjarif@lion.lmu.edu>
2018-04-07 16:35:09 -0700

    stateless tings

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-class Form extends React.Component {
-  render() {
-    return(
-      <form onSubmit = {this.props.get_weather}>
-        <input type="text" name="city" placeholder="City..."/>
-        <input type="text" name="country" placeholder="Country..."/>
-        <button>Get Weather</button>
-
-      </form>
-    );
-  }
-}
+const Form = props => (
+  <form onSubmit = {props.get_weather}>
+    <input type="text" name="city" placeholder="City..."/>
+    <input type="text" name="country" placeholder="Country..."/>
+    <button>Get Weather</button>
+  </form>
+);
 
 export default Form;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,21 +1,34 @@
 import React from 'react';
 
-class Weather extends React.Component {
-  render() {
-    return(
-      //ifelse statements and stuff dont exist in REACT. so we use the following
-      //if this.props.city and this.props.country returns true then we display <p> and so on.
-      <div>
-        { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
-        { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-        { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
-        { this.props.visibility && <p>Visibility: {this.props.visibility}</p> }
-        { this.props.description && <p>Conditions: {this.props.description}</p> }
-        { this.props.error && <p>Error: {this.props.error}</p> }
+const Weather = props => (
+    <div>
+      { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }
+      { props.temperature && <p>Temperature: {props.temperature}</p> }
+      { props.humidity && <p>Humidity: {props.humidity}</p> }
+      { props.visibility && <p>Visibility: {props.visibility}</p> }
+      { props.description && <p>Conditions: {props.description}</p> }
+      { props.error && <p>Error: {props.error}</p> }
+    </div>
+);
 
-      </div>
-    );
-  }
-};
+//The following is a state component. We're trying to make a stateless component
+//Only difference is, we define the function with const weather = ....
+// class Weather extends React.Component {
+//   render() {
+//     return(
+//       //ifelse statements and stuff dont exist in REACT. so we use the following
+//       //if props.city and props.country returns true then we display <p> and so on.
+//       <div>
+//         { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }
+//         { props.temperature && <p>Temperature: {props.temperature}</p> }
+//         { props.humidity && <p>Humidity: {props.humidity}</p> }
+//         { props.visibility && <p>Visibility: {props.visibility}</p> }
+//         { props.description && <p>Conditions: {props.description}</p> }
+//         { props.error && <p>Error: {props.error}</p> }
+//
+//       </div>
+//     );
+//   }
+// };
 
 export default Weather;
